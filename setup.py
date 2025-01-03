4f948531785b415679bf45d0653403af70f2de2e
TianMayCry9 <139460897+TianMayCry9@users.noreply.github.com>
2025-01-03 16:39:18 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="biox",
-    version="1.0.0",
+    version="1.1.0",
     description="A universal biological sequence compressor",
     author="Tianx Xiao",
     author_email="x1058513236l@gmail.com",
